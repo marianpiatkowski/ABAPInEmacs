0f0e7249d4bad9996ecb214394475c9691740957
Marian Piatkowski <marianpiatkowski@web.de>
2021-03-24 22:44:05 +0100

    add template of Where-Used HTTP response

--- test/snippets.el
+++ test/snippets.el
@@ -36,3 +36,42 @@
                                              :data post-data
                                              :params params)))
     ))
+
+
+;;========================================================================
+;; Loading items of search result:
+;;========================================================================
+
+;; # SVER_NATIVE_GENERATE_VERI
+
+
+;; REQUEST_LINE
+;; METHOD:  POST
+;; URI:     /sap/bc/adt/repository/informationsystem/usageSnippets
+;; VERSION: HTTP/1.1
+
+;; HEADER_FIELDS
+;; Content-Type         application/vnd.sap.adt.repository.usagesnippets.request.v1+xml
+;; Accept               application/vnd.sap.adt.repository.usagesnippets.result.v1+xml
+;; X-sap-adt-profiling  server-time
+
+;; MESSAGE_BODY (see where-used/template_request_item1.xml)
+
+;; RESPONSE-MESSAGE_BODY (see where-used/template_response_item1.xml)
+
+
+;; # CL_SVER_DBI_GEN_NATIVE_VERI :: CHECK_TARGET_REPORT_NAMES
+
+;; REQUEST_LINE
+;; METHOD:  POST
+;; URI:     /sap/bc/adt/repository/informationsystem/usageSnippets
+;; VERSION: HTTP/1.1
+
+;; HEADER_FIELDS
+;; Content-Type         application/vnd.sap.adt.repository.usagesnippets.request.v1+xml
+;; Accept               application/vnd.sap.adt.repository.usagesnippets.result.v1+xml
+;; X-sap-adt-profiling  server-time
+
+;; MESSAGE_BODY (see where-used/template_request_item2.xml)
+
+;; RESPONSE-MESSAGE_BODY (see where-used/template_response_item2.xml)
