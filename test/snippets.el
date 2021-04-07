49260c4b0bf37c7dd841a54df9bc5deb544f0b2f
Marian Piatkowski <marianpiatkowski@web.de>
2021-04-07 11:55:16 +0200

    code snippet: work on building ABAP Outline buffer

--- test/snippets.el
+++ test/snippets.el
@@ -1,4 +1,69 @@
 ;;========================================================================
+;; ABAP outline
+;;========================================================================
+
+(defconst abaplib--outline-buffer "*ABAP Outline*"
+  "ABAP Outline buffer");
+
+(defun abaplib-util-outline-buf-write (log)
+  (save-current-buffer
+    (set-buffer (get-buffer-create abaplib--outline-buffer))
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (goto-char (point-min))
+    (insert (format "%s" log))
+    (setq buffer-read-only t)));
+
+(defun abap-outline ()
+  "Get object structure of ABAP development object."
+  (interactive)
+  (let ((object-uri     (abaplib-get-property 'uri))
+        (object-version (abaplib-get-property 'version)))
+    (abaplib-outline object-uri object-version)));
+
+(defun abaplib-outline (object-uri object-version)
+  (message "Getting outline...")
+  (let* ((request-uri (concat object-uri "/objectstructure"))
+         (headers `(("x-csrf-token" . ,(abaplib-get-csrf-token))
+                    ("Accept"       . "application/xml;q=0.8, application/vnd.sap.adt.objectstructure+xml;q=0.9, application/vnd.sap.adt.objectstructure.v2+xml")))
+         (params `((version . ,object-version)
+                   (withShortDescriptions . "true")))
+         (outline (abaplib--rest-api-call request-uri
+                                          nil
+                                          :parser 'abaplib-util-xml-parser
+                                          :headers headers
+                                          :params params)))
+    (abaplib--outline-post outline)));
+
+(defun abaplib--outline-post (outline)
+  "Build outline buffer."
+  (let ((object-name (xml-get-attribute outline 'name))
+        (object-type (xml-get-attribute outline 'type))
+        (main-links  (xml-get-children outline 'link))
+        (object-structure (xml-get-children outline 'objectStructureElement))
+        (output-log))
+    (setq output-log (format "Outline for %s\n\n" object-name))
+    (dolist (elem object-structure)
+      (let ((sub-obj-structure (xml-get-children elem 'objectStructureElement))
+            (links (xml-get-children elem 'link)))
+        (setq output-log (concat output-log "\n"
+                                 (format "  %s %s" (xml-get-attribute elem 'name) (xml-get-attribute elem 'type))))
+        (dolist (link links)
+          ;; do something here
+          )
+        (dolist (sub-elem sub-obj-structure)
+          (let ((sub-links (xml-get-children sub-elem 'link)))
+            (setq output-log (concat output-log "\n"
+                                     (format "    %s %s" (xml-get-attribute sub-elem 'name) (xml-get-attribute sub-elem 'type))))
+            (dolist (sub-link sub-links)
+              ;; do something here
+              )
+
+        ))))
+    (abaplib-util-outline-buf-write output-log)
+    (pop-to-buffer (get-buffer-create abaplib--outline-buffer))));
+
+;;========================================================================
 ;; Snippet - examining ABAP Where-Used list
 ;;========================================================================
 
